1ebd80345869582d7d5287d0ead89668d614fc92
1801a <45133116+1801a@users.noreply.github.com>
2018-11-18 21:30:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1998,7 +1998,7 @@ client.on('message', message => {
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/e3DgVHD**")
+  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/YGFpw9Q**")
      
      
   message.channel.sendEmbed(embed);
