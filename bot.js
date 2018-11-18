8133f54396d4862426d5ea99ce7459e0281b759f
1801a <45133116+1801a@users.noreply.github.com>
2018-11-18 08:30:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -144,52 +144,6 @@ if (message.content.startsWith(prefix + 'help')) {
     }
 });
 
-
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("اي دي سيرفرك - Server id");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        });
-    });
-});
-
-
-
-client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.get("512398650614349848");
-    if (!channel) {
-        console.log("!the channel id it's not correct");
-        return;
-    }
-    if (member.id == client.user.id) {
-        return;
-    }
-    console.log('-');
-    var guild;
-    while (!guild)
-        guild = client.guilds.get("409005637284921374");
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            if (dat[Inv])
-                if (dat[Inv] < Invite.uses) {
- channel.send(`تم دعوته بواسطة  ${Invite.inviter} `) ;         
- }
-            dat[Inv] = Invite.uses;
-       
-       });
-    });
-});
-
-
-
-
-
-
 client.on('message' , message => {
   var prefix = "-";
   if(message.author.bot) return;
