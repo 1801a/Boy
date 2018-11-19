661f7d8a6d7e3156c0bf861cd16e2d9d7c57cd83
1801a <45133116+1801a@users.noreply.github.com>
2018-11-20 00:19:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -475,7 +475,7 @@ client.on('message', msg => {
 
 client.on('message', msg => {
   if (msg.content.startsWith('-play')) {
-    msg.channel.send('Use >play');
+    msg.channel.send('كتب و اسم اغنية -play');
   }
 });
 
