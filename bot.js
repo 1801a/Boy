04cb07fe2c0b899f6689fc8448195c7da397fbcc
1801a <45133116+1801a@users.noreply.github.com>
2018-11-20 11:16:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -253,9 +253,10 @@ client.on('message', function(message) {
         message.reply (' ')
            if(!message.channel.guild) return message.reply('** This command only for servers**');
      message.member.addRole(message.guild.roles.find('name', 'Muted')); 
+    const embed500 = new Discord.RichEmbed()
       .setTitle(":x: | تمت معاقبتك")
             .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
-      .addField(`by`,`JoyBOT`)
+      .addField(`BY`,`JoyBOT`)
             .setColor("c91616")
             .setThumbnail(`${message.author.avatarURL}`)
             .setAuthor(message.author.username, message.author.avatarURL) 
@@ -266,8 +267,6 @@ client.on('message', function(message) {
     }
     }
 })
-
-
 
 
 client.on('ready', () => {
