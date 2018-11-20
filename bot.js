e9dde607d72241558528a622fb1cb4025a0218da
1801a <45133116+1801a@users.noreply.github.com>
2018-11-21 00:41:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1033,20 +1033,6 @@ if(!args[0]) return message.reply('Write Some Things');
 });
 
 
-client.on("message", message => {
-    if (message.content.startsWith("-bc")) {
-                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
-  m.send(`${argresult}\n ${m}`);
-  })
-  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
-  message.delete();
-  };
-  });
-
-
 let points = {};
 const type = [
     {
